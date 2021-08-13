43d661cbdec7127997536233611e7339ae40b1f8
callmehHM <86656271+callmehHM@users.noreply.github.com>
2021-08-13 09:56:10 +0530

    Create Find The Array_Codeforces.cpp

---
+++ Find The Array_Codeforces.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{int t;
+cin>>t;
+while(t--){
+int n;
+cin>>n;
+if(floor(sqrt(n))==sqrt(n))
+{cout<<floor(sqrt(n))<<"\n";}
+else{cout<<floor(sqrt(n))+1<<"\n";}
+   
+}  
+ return 0;
+}
