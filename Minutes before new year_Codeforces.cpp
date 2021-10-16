057894f35a7d354e68a02dd8bbc44ada95be1acc
callmehHM <86656271+callmehHM@users.noreply.github.com>
2021-10-16 14:57:42 +0530

    Create Minutes before new year_Codeforces.cpp

---
+++ Minutes before new year_Codeforces.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   int t;
+   cin>>t;
+   while(t--)
+   {
+       int h,m;
+       cin>>h>>m;
+       cout<<60*(24-h)-m<<endl;
+             
+   }
+ return 0;
+
+}
